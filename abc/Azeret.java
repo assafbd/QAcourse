771c9b7c342a279dba4ec834045d22cd1f47ed2e
jbt <jbt@411-INS>
2023-02-05 21:32:21 +0200

    Adding Azeret Class

--- abc/Azeret.java
+++ abc/Azeret.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
 
 public class Azeret {
 
 	public static void main(String[] args) {
 		//Azeret - Factorial
 		// Int Number
-		int num = 5;
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number you want to Azeret - Factorial: ");
+        int num = scanner.nextInt();
+        
 		int azeret=1;
 		 while (num > 1) {
+		    	System.out.println(azeret + " * "+ num+" = "+ azeret*num);
 		        azeret = azeret*num;
 		        num--;
+		    	System.out.println("total " + azeret +" Inside the loop");
+		    	System.out.println("**********************************");
+
 		    }
+	System.out.println(azeret);
+	scanner.close();
 	}
 
 }
